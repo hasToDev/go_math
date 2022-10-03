2e9f793cfcef45a27073ffcddad1c82cc05047d3
hasToDev <hasdikus@gmail.com>
2022-10-03 21:53:39 +0700

    transform Add into a variadic function

---
+++ v2/calc/calc.go
@@ -0,0 +1,17 @@
+package calc
+
+func Add(args ...int) int {
+	s := 0
+	for _, v := range args {
+		s += v
+	}
+	return s
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
